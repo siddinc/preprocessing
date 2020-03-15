be8384770e5e27f82d8161c85ee3048d5563c144
siddinc <sanghavisidd@gmail.com>
2020-03-15 14:26:17 +0530

    initial commit

---
+++ rank_accuracy_calculator.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def rank_accuracy_calculator(preds, labels):
+  rank1 = 0
+  rank5 = 0
+
+  for (p, gt) in zip(preds, labels):
+    p = np.argsort(p)[::-1]
+
+    if gt in p[:5]:
+      rank5 += 1
+
+    if gt == p[0]:
+      rank1 += 1
+  
+  rank1 /= float(len(labels))
+  rank5 /= float(len(labels))
+  return (rank1, rank5)
